2d52719419c2ead97fe5e04a420f475df3718d48
Bishwash <bishwash312@gmail.com>
2026-01-21 16:50:58 +0545

    database fix

--- src/modules/vendors/validators.ts
+++ src/modules/vendors/validators.ts
@@ -1,31 +1,30 @@
 import z from "zod";
 const loginValidationSchema = z.object({
-	username: z.string().nonempty(),
-	password: z.string().nonempty(),
-	deviceToken: z.string().nonempty(),
-	host: z.string().nonempty(),
-	userAgent: z.string().nonempty(),
+  username: z.string().nonempty(),
+  password: z.string().nonempty(),
+  deviceToken: z.string().nonempty(),
+  host: z.string().nonempty(),
+  userAgent: z.string().nonempty(),
 });
 const changePasswordValidationSchema = z.object({
-	currentPassword: z.string().min(8).nonempty(),
-	newPassword: z.string().min(8).nonempty(),
-	confirmPassword: z.string(),
+  currentPassword: z.string().min(8).nonempty(),
+  newPassword: z.string().min(8).nonempty(),
+  confirmPassword: z.string(),
 });
 const updateRetailerInfo = z.object({
-	retailer_id: z.string().nonempty(),
-	commission_rate: z.number(),
-	verify: z.boolean(),
+  retailer_id: z.string().nonempty(),
+  commission_rate: z.number(),
+  verify: z.boolean(),
 });
 const validationSchema = z.object({
-	name: z.string().nonempty(),
-	username: z.string().nonempty(),
-	email: z.string().nonempty(),
-	password: z.string().min(8).nonempty(),
-
+  name: z.string().min(1, "Name is required"),
+  username: z.string().min(1, "Username is required"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
 });
 export {
-	updateRetailerInfo,
-	loginValidationSchema,
-	changePasswordValidationSchema,
-	validationSchema,
+  updateRetailerInfo,
+  loginValidationSchema,
+  changePasswordValidationSchema,
+  validationSchema,
 };
