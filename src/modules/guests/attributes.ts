0a2931d8118e6150a3c3eb2d32f32a0812a7da5a
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-01-26 21:09:02 +0545

    feature in the guest user

--- src/modules/guests/attributes.ts
+++ src/modules/guests/attributes.ts
@@ -1,17 +1,18 @@
-import { integer, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { integer, serial, text, timestamp, boolean } from "drizzle-orm/pg-core";
 const tableName = "Guests";
 // import events from "@/modules/events/schema" // Missing module
 const attributes = {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  eventId: integer("eventId").notNull(), // .references(() => events.id), // Event has the guests - disabled due to missing schema
-  email: text("email"),
-  address: text("address"),
-  relation: text("relation"),
-  phone: text("phone"),
-  // modules:,
-  createdAt: timestamp("createdAt").defaultNow(),
-  updatedAt: timestamp("updatedAt").defaultNow(),
+	id: serial("id").primaryKey(),
+	name: text("name").notNull(),
+	eventId: integer("eventId").notNull(), // .references(() => events.id), // Event has the guests - disabled due to missing schema
+	email: text("email"),
+	address: text("address"),
+	relation: text("relation"),
+	isDeleted: boolean("isDeleted").notNull().default(false),
+	phone: text("phone"),
+	// modules:,
+	createdAt: timestamp("createdAt").defaultNow(),
+	updatedAt: timestamp("updatedAt").defaultNow(),
 };
 
 export { tableName, attributes };
