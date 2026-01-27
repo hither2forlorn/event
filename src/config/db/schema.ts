9ce7e158a980d9c8d7359d06038e2cd6673bf028
Bishwash <bishwash312@gmail.com>
2026-01-27 11:47:20 +0545

    test push

--- src/config/db/schema.ts
+++ src/config/db/schema.ts
@@ -2,5 +2,5 @@ import adminSchema from "../../modules/admin/schema";
 import venturesSchema from "../../modules/vendors/schema";
 import category from "../../modules/category/schema";
 import user, { roleEnum } from "../../modules/user/schema";
-import event from "../../modules/event/schema";
-export { adminSchema, venturesSchema, category, user, roleEnum, event };
+import eventSchema, { eventType } from "../../modules/event/schema";
+export { adminSchema, venturesSchema, category, user, roleEnum, eventSchema, eventType };
--- src/modules/event/schema.ts
+++ src/modules/event/schema.ts
@@ -1,5 +1,6 @@
 import { pgTable } from "drizzle-orm/pg-core";
-import { attributes, tableName } from "./attributes";
+import { attributes, tableName, eventType } from "./attributes";
 
 const schema = pgTable(tableName, attributes);
+export { eventType };
 export default schema;
