0827cdc6eef1fbf9fbd78aafa4cd2e0c639d450c
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-01-28 11:26:46 +0545

    update in the user and the gueust maping with the foriegn key

--- src/modules/user/resource.ts
+++ src/modules/user/resource.ts
@@ -1,29 +1,31 @@
 import { roleEnum } from "./attributes";
 
 export interface UserColumn {
-  id: number;
-  info: any;
-  password: string;
-  email: string;
-  role: typeof roleEnum;
-  createdAt: Date;
-  infos: any;
+	id: number;
+	info: any;
+	password: string;
+	email: string;
+	phone: string;
+	role: typeof roleEnum;
+	createdAt: Date;
+	infos: any;
 }
 class Resource {
-  static toJson(user: UserColumn): Partial<UserColumn> | null {
-    if (!user) return null;
-    const data: Partial<UserColumn> = {
-      id: user.id,
-      info: user.info,
-      email: user.email,
-      role: user.role,
-      createdAt: user.createdAt,
-    };
-    return data;
-  }
-  static collection(users: UserColumn[]) {
-    return users.map(this.toJson);
-  }
+	static toJson(user: UserColumn): Partial<UserColumn> | null {
+		if (!user) return null;
+		const data: Partial<UserColumn> = {
+			id: user.id,
+			phone: user.phone,
+			info: user.info,
+			email: user.email,
+			role: user.role,
+			createdAt: user.createdAt,
+		};
+		return data;
+	}
+	static collection(users: UserColumn[]) {
+		return users.map(this.toJson);
+	}
 }
 
 export default Resource;
