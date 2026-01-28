2e4eeb17bc9a270a96cd269b338a9b51fe366939
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-01-28 11:26:46 +0545

    update in the user and the gueust maping with the foriegn key

--- src/modules/guests/attributes.ts
+++ src/modules/guests/attributes.ts
@@ -1,12 +1,11 @@
+import user from "@/modules/user/schema"
 import { integer, serial, text, timestamp, boolean } from "drizzle-orm/pg-core";
-const tableName = "Guests";
+const tableName = "guests";
 // import events from "@/modules/events/schema" // Missing module
 const attributes = {
 	id: serial("id").primaryKey(),
-	name: text("name").notNull(),
+	userId: integer("userId").references(() => user.id),
 	eventId: integer("eventId").notNull(), // .references(() => events.id), // Event has the guests - disabled due to missing schema
-	email: text("email"),
-	address: text("address"),
 	relation: text("relation"),
 	isDeleted: boolean("isDeleted").notNull().default(false),
 	phone: text("phone"),
--- src/modules/user/attributes.ts
+++ src/modules/user/attributes.ts
@@ -4,15 +4,16 @@ const roleEnum = pgEnum("role", ["client", "vendor"]);
 
 const tableName = "users";
 const attributes = {
-  id: serial("id").primaryKey(),
-  email: text("email").notNull().unique(),
-  name: text("name").notNull(),
-  password: text("password").notNull(),
-  info: json("info"),
-  role: roleEnum("role").notNull().default("client"),
-  // modules:,
-  createdAt: timestamp("createdAt").defaultNow(),
-  updatedAt: timestamp("updatedAt").defaultNow(),
+	id: serial("id").primaryKey(),
+	email: text("email").notNull().unique(),
+	name: text("name").notNull(),
+	phone: text("phone").notNull(),
+	password: text("password").notNull(),
+	info: json("info"),
+	role: roleEnum("role").notNull().default("client"),
+	// modules:,
+	createdAt: timestamp("createdAt").defaultNow(),
+	updatedAt: timestamp("updatedAt").defaultNow(),
 };
 
 export { tableName, attributes, roleEnum };
