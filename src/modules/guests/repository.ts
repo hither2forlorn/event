f06b43f608c88c5de2d12fde2339b5706d9d8196
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-01-29 11:32:08 +0545

    update in this mosule

--- src/modules/guests/repository.ts
+++ src/modules/guests/repository.ts
@@ -1,14 +1,33 @@
+import user from "@/modules/user/schema";
 import guests from "./schema";
+import event from "@/modules/event/schema"
 const selectQuery = {
 	id: guests.id,
-	email: guests.email,
-	name: guests.name,
-	address: guests.address,
+	email: user.email,
+	name: user.name,
+	address: user.address,
 	relation: guests.relation,
-	phone: guests.phone,
+	foodPreference: user.foodPreference,
+	location: {
+		country: user.country,
+		zip_code: user.zip,
+		address: user.address
+	},
+	role: user.role,
+	phone: user.phone,
 	createdAt: guests.createdAt,
 	updatedAt: guests.updatedAt,
 };
+const selectWithEvent = {
+	...selectQuery,
+	event_detail: {
+		eventTitle: event.title,
+		event_venue: event.location,
+		event_start: event.startDate,
+		event_end: event.endDate
+	}
+}
 export default {
 	selectQuery,
+	selectWithEvent
 };
