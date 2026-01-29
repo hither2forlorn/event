3c44f8895363b731fd15038a9f7facd280ca0f5f
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2026-01-29 17:09:58 +0545

    update in the event

--- src/modules/event/route.ts
+++ src/modules/event/route.ts
@@ -1,51 +1,51 @@
 import Controller from "./controller";
 
 const routes = [
-  {
-    method: "get",
-    controller: Controller.get,
-    path: "event",
-  },
-  {
-    method: "post",
-    controller: Controller.create,
-    path: "event",
-    authorization: true,
-    authCheckType: ["client"],
-  },
-  {
-    method: "get",
-    controller: Controller.listMyEvents,
-    path: "event/my-events",
-    authorization: true,
-    authCheckType: ["client"],
-  },
-  {
-    method: "get",
-    controller: Controller.getUserRelatedToEvent,
-    path: "event/:eventId/users",
-    authorization: true,
-    authCheckType: ["client"],
-  },
-  {
-    method: "get",
-    controller: Controller.findOne,
-    path: "event/:id",
-  },
-  {
-    method: "patch",
-    controller: Controller.update,
-    path: "event/:id",
-    authorization: true,
-    authCheckType: ["client"],
-  },
-  {
-    method: "delete",
-    controller: Controller.deleteModule,
-    path: "event/:id",
-    authorization: true,
-    authCheckType: ["client"],
-  },
+	{
+		method: "get",
+		controller: Controller.get,
+		path: "event",
+	},
+	{
+		method: "post",
+		controller: Controller.create,
+		path: "event",
+		authorization: true,
+		authCheckType: ["client"],
+	},
+	{
+		method: "get",
+		controller: Controller.listMyEvents,
+		path: "event/my-events",
+		authorization: true,
+		authCheckType: ["client"],
+	},
+	{
+		method: "get",
+		controller: Controller.getUserRelatedToEvent,
+		path: "event/:eventId/users",
+		authorization: true,
+		authCheckType: ["client"],
+	},
+	{
+		method: "get",
+		controller: Controller.findOne,
+		path: "event/:id",
+	},
+	{
+		method: "patch",
+		controller: Controller.update,
+		path: "event/:id",
+		authorization: true,
+		authCheckType: ["client"],
+	},
+	{
+		method: "delete",
+		controller: Controller.deleteModule,
+		path: "event/:id",
+		authorization: true,
+		authCheckType: ["client"],
+	},
 ];
 
 export default routes;
