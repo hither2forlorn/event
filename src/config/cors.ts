22bba720dc5dcf26c9768839c158ac2f167375e6
Bishwash <bishwash312@gmail.com>
2026-02-01 12:36:14 +0545

    added the endpoint in cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,18 @@
 import cors from "cors";
 
 export const availableCors = [
-	"http://localhost:3000",
-	"http://localhost:9000"
+  "http://localhost:3000",
+  "http://localhost:9000",
+  "http://localhost:8081",
 ];
 
 const corsOptions = cors({
-	origin: availableCors,
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-	allowedHeaders: ["Content-Type", "Authorization", "Api-Key", "isGuestMode"],
-	credentials: true,
-	exposedHeaders: ["Authorization", "Api-Key"],
-	maxAge: 86400
+  origin: availableCors,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "Api-Key", "isGuestMode"],
+  credentials: true,
+  exposedHeaders: ["Authorization", "Api-Key"],
+  maxAge: 86400,
 });
 
-export default corsOptions; 
+export default corsOptions;
